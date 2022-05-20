8014a5b20c68db66bac74254f7914fb4fbb4d851
IT21176838 <99307615+IT21176838@users.noreply.github.com>
2022-05-20 20:15:35 +0530

    Create interview.cpp

---
+++ interview.cpp
@@ -0,0 +1,69 @@
+#include <interview.h>
+
+void Interview::setInterviewId(int interId)
+{
+	interviewId= interId;
+	cout << "Interview ID :" ;
+	cin >> interviewId;
+
+}
+int Interview::getdisplayID()
+{
+	cout << "Interview ID :" << interviewId << endl;
+	return 0;
+}
+
+
+void Interview::setinterviewType(string interType)
+{
+	interviewType= interType;
+	cout << "Interview Type :";
+	cin >> interviewType;
+	
+
+}
+int Interview::getdisplayTYPED()
+{
+	cout << "Interview Type :" << interviewType << endl;
+	return 0;
+}
+
+
+void Interview::setinterviewDate(string interDate)
+{
+	interviewDate= interDate;
+	cout << "Interview Date :";
+	cin >> interviewDate;
+
+}
+int Interview::getdisplayDATE()
+{
+	cout << "Interview Date :" << interviewDate << endl;
+	return 0;
+}
+
+void Interview::setinterviewTime(string interTime)
+{
+	interviewTime= interTime;
+	cout << "Interview Time :" ;
+	cin >> interviewTime;
+
+}
+int Interview::getdisplayTIME()
+{
+	cout << "Interview Time :" << interviewTime << endl;
+	return 0;
+}
+
+void Interview::setinterviewCompany(string interCom)
+{
+	interviewCompany= interCom;
+	cout << "Interview Company :" ;
+	cin >> interviewCompany;
+
+}
+int Interview::getdisplayCOMPANY()
+{
+	cout << "Interview Company :" << interviewCompany << endl;
+	return 0;
+}
